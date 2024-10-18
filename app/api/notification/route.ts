aeca0766ba395bf3c25ebb797fdef9308536d5ae
Fiammamuscari <fiammamuscari@gmail.com>
2024-10-18 01:09:03 -0300

    🧃 notification webhook update

--- app/api/notification/route.ts
+++ app/api/notification/route.ts
@@ -1,6 +1,4 @@
-// notification.ts
-
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 import { createAppointment } from "@/actions/appointments";
 import { MercadoPagoConfig, Payment } from "mercadopago";
 
@@ -8,29 +6,56 @@ const client = new MercadoPagoConfig({
   accessToken: process.env.MP_ACCESS_TOKEN!,
 });
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const paymentId = req.body.data.id; // ID del pago recibido
-  const payment = await new Payment(client).get({ id: paymentId });
+export async function POST(req: Request) {
+  const body = await req.json();
+  const paymentId = body.data?.id;
 
-  if (payment.status === "approved") {
-    const { userName, userEmail, date, time, services } = payment.metadata;
-
-    const appointmentData = {
-      userName,
-      userEmail,
-      date,
-      time,
-      services,
-    };
-
-    const appointmentResult = await createAppointment(appointmentData);
-
-    if (appointmentResult.error) {
-      return res.status(500).json({ error: appointmentResult.error });
-    }
-
-    return res.status(200).json({ success: true });
+  if (!paymentId) {
+    return NextResponse.json(
+      { error: "ID de pago no encontrado en la solicitud" },
+      { status: 400 }
+    );
   }
 
-  return res.status(400).json({ error: "Pago no aprobado" });
+  try {
+    const payment = await new Payment(client).get({ id: paymentId });
+
+    if (payment.status === "approved") {
+      const { userName, userEmail, date, time, services } = payment.metadata;
+
+      if (!userName || !userEmail || !date || !time || !services) {
+        return NextResponse.json(
+          { error: "Faltan datos necesarios en la metadata" },
+          { status: 400 }
+        );
+      }
+
+      const appointmentData = {
+        userName,
+        userEmail,
+        date,
+        time,
+        services,
+      };
+
+      const appointmentResult = await createAppointment(appointmentData);
+
+      if (appointmentResult.error) {
+        return NextResponse.json(
+          { error: appointmentResult.error },
+          { status: 403 }
+        );
+      }
+
+      return NextResponse.json({ success: true }, { status: 200 });
+    }
+
+    return NextResponse.json({ error: "Pago no aprobado" }, { status: 403 });
+  } catch (error) {
+    console.error("Error procesando el pago:", error);
+    return NextResponse.json(
+      { error: "Error interno del servidor" },
+      { status: 403 }
+    );
+  }
 }
